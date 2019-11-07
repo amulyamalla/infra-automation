07737ae199e865f6d71b90d66b4617d6faca5e4c
apmalla <amulyaprasad.malla@gmail.com>
2019-11-08 02:08:21 +0530

    code updated

--- ansible/roles/docker/docker-images-clean.sh
+++ ansible/roles/docker/docker-images-clean.sh
@@ -6,7 +6,7 @@
 		##########################################################################
 
 set -u
-nexus_url=172.23.12.160:8083/
+nexus_url=192.23.12.160:8083/
 num_of_images_keep=5
 for imagename in `docker images --format "{{.Repository}}" | grep "${nexus_url}" | sort | uniq`; do
     for img in `docker images --format "{{.ID}}" --filter reference="$imagename" | tail -n+$((${num_of_images_keep} + 1))`; do
